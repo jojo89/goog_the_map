3777e8cc55131a971d02e294dcb7243866269d8a
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-29 16:02:28 -0500

    no encapsulation

--- public/js/application.js
+++ public/js/application.js
@@ -1,83 +1,176 @@
 
 
-var geocoder;
-  var map;
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var mapOptions = {
-      zoom: 8,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  }
-
-  function codeAddress() {
-    var address = document.getElementById('zip').value;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        console.log(results)
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-        marker.setMap(map);
-        var infoWindowOptions = {
-          content:'moscov'
-        };
-        var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-
-        console.log(marker);
-
-        google.maps.event.addListener(marker,'click', function(e){ 
-          infoWindow.open(map,marker);  
-        });
-     fuction 
-
-
-        
-          
-   
-     
-
-
-
-          
-
-
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
-    });
-  }
 
 
 
 
+  // // working code!
+
+  // var geocoder;
+  // var map;
+
+  // function initialize() {
+  //   geocoder = new google.maps.Geocoder();
+  //   var latlng = new google.maps.LatLng(-34.397, 150.644);
+  //   var mapOptions = {
+  //     zoom: 8,
+  //     center: latlng,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   }
+  //   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  // }
+
+
+  // function codeAddress() {
+  //   var address = document.getElementById('zip').value;
+  //   geocoder.geocode( { 'address': address}, function(results, status) {
+  //     if (status == google.maps.GeocoderStatus.OK) {
+  //       console.log(results)
+  //       map.setCenter(results[0].geometry.location);
+  //       var marker = new google.maps.Marker({
+  //           map: map,
+  //           position: results[0].geometry.location
+  //       });
+  //       var infoWindowOptions = {
+  //         content:'moscov'
+  //       };
+  //       var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+  //       google.maps.event.addListener(marker,'click', function(e){ 
+  //         infoWindow.open(map,marker);  
+  //       });
+  //     } else {
+  //       alert("Geocode was not successful for the following reason: " + status);
+  //     }
+  //   });
+  // }
+
+
+
+// $(document).ready(function() {
+//   google.maps.event.addDomListener(window, 'load', initialize);
+
+
+//   console.log(map);
+
+//   $('#zip_srch').on('click', function(){
+//     codeAddress();
+//   })
+
+
+// });
+
+
+
+  // ///////////////////////////////////////////////////////////////////////////////
+
+// broke ass code
 
 
 
 
 
+  // var acOptions = {
+  //   types: ['establishment']
+  // }
 
+  // var autocomplete = new google.maps.places.Autocomplete(document.getElementById('zip'), acOptions);
+  // autocomplete.bindTo('bounds',map);
+
+  // google.maps.event.addListener(autocomplete,'places_changed', function(){
+  //   infoWindow.close();
+  //   var place = autocomplete.getPlace();
+  //   if (place.geometry.viewport){
+  //     map.fitBounds(place.geometry.viewport);
+  //   }else{
+  //     map.setCenter(place.geometry.location);
+  //     map.setZoom(17);
+  //   }
+  // });  
+
+
+  //   marker.setPosition(place.geometry.location);
+  //   infoWindow.setContent('<div><strong>+ place.name +</strong><br>');
+  //   infoWindow.open(map,marker);
+  //   google.maps.event.addListener(marker,click)  
+  //   }
+  // })
 
 
 
 
 $(document).ready(function() {
-  google.maps.event.addDomListener(window, 'load', initialize);
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
 
-     console.log(geocoder)
-  //   loadScript();
-  $('#zip_srch').on('click', function(){
+  var mapOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
 
-    codeAddress();
-  })
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // var marker = new google.maps.Marker({
+  //   map: map,
+  //   position: latlng
+  // })
+  // marker.setMap(map)
+  
+  // var infoWindow = new google.maps.InfoWindow({
+  //   content:'moscov'
+  // });
+
+  // google.maps.event.addListener(marker,'click', function(e){ 
+  //   infoWindow.open(map, marker);  
+  // });
+
+  var acOptions = {
+    types: ['establishment']
+  }
+  var autocomplete = new google.maps.places.Autocomplete(document.getElementById('zip'), acOptions);
+  autocomplete.bindTo('bounds',map);
+  
+  var infoWindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map
+  });
+
+  
+  google.maps.event.addListener(autocomplete,'place_changed', function(){
+    infoWindow.close();
+    var place = autocomplete.getPlace();
+    console.log(place)
+    if (place.geometry.viewport){
+      map.fitBounds(place.geometry.viewport);
+    }else{
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+    marker.setPosition(place.geometry.location);
+    infoWindow.setContent('<div><strong>+ place.name +</strong><br>');
+    
+    google.maps.event.addListener(marker,'click',function(e){
+      infoWindow.open(map, marker);  
+
+    });  
+  });    
+
+
+  
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

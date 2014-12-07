63bf1c2e5b09e7dac6c29baaf428741315b6e56b
Joseph Burtoft <burtoftallbiz@gmail.com>
2014-12-07 16:09:49 -0500

    ill refactor

--- public/js/application.js
+++ public/js/application.js
@@ -36,15 +36,11 @@ function setMarker(lat, lon, details) {
 }
 
 
-  function codeAddress() {
-    var address = document.getElementById('zip').value;
+  function codeAddress(address) {
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
-        console.log(results[0].geometry.viewport)
         map.setCenter(results[0].geometry.location);
-        console.log(map.getCenter().lng());
-        console.log(map.getCenter().lat());
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
@@ -60,50 +56,24 @@ function setMarker(lat, lon, details) {
         var phrase = document.getElementById('phrase').value;
      
         $.post('/twitter',{lng:lng,lat:lat,phrase:phrase},function(response){
-          console.log(response)
           jQuery.each(response,function(i,val){
-            var marker = makeMarker(val)
-            var infoBox = makeBox(val)
-            var spot = new Spot(marker, infoBox, val.background_image)
-            google.maps.event.addListener(marker,'click', function(e){
+            var spot = new Spot(val.latitude, val.longitude, val.profile_image, val.text, val.background_image)
+            google.maps.event.addListener(spot.marker,'click', function(e){
               spot.openBox()
             });      
           }); 
         })
     }
 
-    function makeMarker(tweetInfo){
-    	return new google.maps.Marker({
-    	    map: map,
-          icon:  tweetInfo.profile_image,
-          position: new google.maps.LatLng(tweetInfo.latitude, tweetInfo.longitude),
-    	    visible: true
-    	});
-    }
-    
-    function makeBox(tweetInfo){
-    	return new InfoBox({
-        content: '<div style="background-repeat:no-repeat;background:white; padding: 10px 30px; color:blue; border-radius:150px; ">'+tweetInfo.text+'</div>'
-    		,disableAutoPan: false
-    		,maxWidth: 0
-    		,pixelOffset: new google.maps.Size(-140, 0)
-    		,zIndex: null
-    		,boxStyle: { 
-    		 }
-    		,closeBoxMargin: "10px 2px 2px 2px"
-    		,closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif"
-    		,infoBoxClearance: new google.maps.Size(1, 1)
-    		,isHidden: false
-    		,pane: "floatPane"
-    		,enableEventPropagation: false
-    	});
-    }
+
 
 
 
 $(document).ready(function() {
+  
   $('#zip_srch').on('click',function(){
-    codeAddress();
+    var address = document.getElementById('zip').value;
+    codeAddress(adress);
   });
     $('#find_tweets').on('click',function(){
      fetch(); 
--- public/js/spot.js
+++ public/js/spot.js
@@ -1,6 +1,6 @@
-var Spot = function (marker, infoBox, background) {
-  this.marker = marker;
-  this.infoBox = infoBox;
+var Spot = function (latitude, longitude, profile_image, text, background) {
+  this.marker = this.makeMarker(latitude, longitude, profile_image);
+  this.infoBox = this.makeBox(text);
   this.background = background;
 }
 
@@ -23,4 +23,31 @@ Spot.prototype.popBox = function() {
   window.setTimeout(function(){
     _this.closeBox()
   }, 2000);
+}
+
+Spot.prototype.makeMarker = function(latitude, longitude, profile_image){
+  return new google.maps.Marker({
+      map: map,
+      icon: profile_image,
+      position: new google.maps.LatLng(latitude, longitude),
+      visible: true
+  });
+}
+
+Spot.prototype.makeBox = function(text){
+  return new InfoBox({
+    content: '<div style="background-repeat:no-repeat;background:white; padding: 10px 30px; color:blue; border-radius:150px; ">'+text+'</div>'
+    ,disableAutoPan: false
+    ,maxWidth: 0
+    ,pixelOffset: new google.maps.Size(-140, 0)
+    ,zIndex: null
+    ,boxStyle: {
+     }
+    ,closeBoxMargin: "10px 2px 2px 2px"
+    ,closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif"
+    ,infoBoxClearance: new google.maps.Size(1, 1)
+    ,isHidden: false
+    ,pane: "floatPane"
+    ,enableEventPropagation: false
+  });
 }

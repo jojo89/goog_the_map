75e9e139681819d0a4848865c2e801f6dcce804f
Joseph Burtoft <burtoftallbiz@gmail.com>
2015-01-11 12:45:12 -0500

    Added clear list

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {  
+  var list = new List($('#list ol'));
   Map.makeMap();
   $('#zip_srch').on('click',function(){
     var address = document.getElementById('zip').value;
     Map.codeAddress(address);
   });
   $('#find_tweets').on('click',function(){
-    var list = new List($('#list ol'));
     Map.fetch(list); 
   });
   $('#clear-tweet').on('click',function(){
+     list.clear()
      Map.clearMap()
   });
 });

1819805ac89fc538a2a01760d1a6939602098b9d
Joseph Burtoft <burtoftallbiz@gmail.com>
2015-01-13 09:20:00 -0500

    Changed list ob lit

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {  
-  var list = new List($('#list ol'));
+$(document).ready(function() {
+  var List = list()
   Map.makeMap();
   $('#zip_srch').on('click',function(){
     var address = document.getElementById('zip').value;
     Map.codeAddress(address);
   });
   $('#find_tweets').on('click',function(){
-    Map.fetch(list); 
+    Map.fetch(List); 
   });
   $('#clear-tweet').on('click',function(){
-     list.clear()
+     List.clear()
      Map.clearMap()
   });
 });
--- public/js/list.js
+++ public/js/list.js
@@ -1,35 +1,30 @@
-var List = function (html) {
-  this.html = html;
-}
-
-List.prototype.replaceItem = function(index) {
-  $(this.listItems()[index]).replaceWith(this.blank())
-}
-
-List.prototype.addToList = function(spot, index) {
-  if (this.listItemAtIndex(index) != undefined){
-    $(this.listItemAtIndex(index)).replaceWith(this.tweet(spot))
+function list(){
+  return {
+    html: $('#list ol'),
+    replaceItem: function(index){
+      $(this.listItems()[index]).replaceWith(this.blank())
+    },
+    addToList: function(spot, index){
+      if (this.listItemAtIndex(index) != undefined){
+        $(this.listItemAtIndex(index)).replaceWith(this.tweet(spot))
+      }
+    },
+    clear: function(){
+      for (i = 0; i < 10; i++) {
+        this.replaceItem(i)
+      }
+    },
+    tweet: function(spot){
+      return "<li><a href='javascript:void(0)'>" + "<u><b>" + spot.user + "</b></u>" + "- " + spot.text.trunc(17) + "... " + spot.retweet_count + "</a></li>"
+    },
+    blank: function(){
+      return "<li><a href='javascript:void(0)'></a></li>"
+    },
+    listItems: function(){
+      return this.html.find('li')
+    },
+    listItemAtIndex: function(index){
+     return this.listItems()[index]
+    }
   }
-}
-
-List.prototype.clear = function() {
-  for (i = 0; i < 10; i++) {
-    this.replaceItem(i)
-  }
-}
-
-List.prototype.tweet = function(spot){
-  return "<li><a href='javascript:void(0)'>" + "<u><b>" + spot.user + "</b></u>" + "- " + spot.text.trunc(17) + "... " + spot.retweet_count + "</a></li>"
-}
-
-List.prototype.blank = function(){
-  return "<li><a href='javascript:void(0)'></a></li>"
-}
-
-List.prototype.listItems = function(){
-  return this.html.find('li')
-}
-
-List.prototype.listItemAtIndex = function(index){
-  return this.listItems()[index]
 }

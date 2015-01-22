756ecd75a4101b686493451577c6be494aa26f2b
Joseph Burtoft <burtoftallbiz@gmail.com>
2015-01-21 21:04:44 -0500

    added some angular stuff

--- public/js/application.js
+++ public/js/application.js
@@ -13,3 +13,50 @@ $(document).ready(function() {
      Map.clearMap()
   });
 });
+
+
+  var app;
+  var app = angular.module('tweetFinder', []);
+  var initTweet = [
+    { 
+      name: 'Jodeza',
+      tweet: "Can't believe my cat right now. hashtag whatabitch lol.", 
+      canShow: true, 
+      price: 45, 
+      image: "https://pbs.twimg.com/profile_images/3221004065/743ee2b5082dc27ac647e8f3ee18a0f0_400x400.gif" 
+    }, 
+    { 
+      name: 'lint', 
+      tweet: "My Mom be trippin. She ain't even aight.", 
+      canShow: true, 
+      price: 34,
+      image: "https://pbs.twimg.com/profile_images/3221004065/743ee2b5082dc27ac647e8f3ee18a0f0_400x400.gif" 
+    }
+  ];
+
+  app.controller('ListController', function(){
+    this.tweets = initTweet;
+   });
+   
+  app.controller('ItemController', function(){
+    this.item = 1;
+
+    this.setItem = function(item){
+     this.item = item;
+    };
+     
+    this.isSet = function(item){
+      return this.item === item
+    };
+  });
+  
+  app.controller('FormController', function(){
+    this.object = {stars: 4, text: "j", person: "rich keef"};
+    this.reviews = [{stars: 5, text: "ferret", person: "chief joseph"}, {stars: 4, text: "annual", person: "chief keef"}];
+    this.addReview = function(review){
+      this.object.createdAt = Date.now()
+      this.reviews.push(this.object)
+      this.object = {};
+    }
+  
+  })

844160aca43945ebc61360bb19a7c0b7a6591b91
Joseph Burtoft <burtoftallbiz@gmail.com>
2015-02-11 21:17:19 -0500

    refactor the isn

--- public/js/application.js
+++ public/js/application.js
@@ -1,59 +1,58 @@
-  var app;
-  var app = angular.module('tweetFinder', ['ngResource','dep', 'ngRoute']);
+var app;
+var app = angular.module('tweetFinder', ['ngResource', 'dep', 'ngRoute']);
 
-   app.controller('MapController', function($http, $scope, spotFactory, Twitter, momo, mapi, $q, fetchAll){
+app.controller('MapController', function($http, $scope, spotFactory, twitterSinatraApi, Query, $q, fetchAll, gMap) {
     controller = this
     controller.tweets = [];
-    controller.mapOptions = {
-      zoom: 8,
-      center: new google.maps.LatLng(34.397, -118.644),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    controller.makeMap = function(){
-      controller.map = new google.maps.Map(document.getElementById("map-canvas"), this.mapOptions);
-      controller.latitude = this.map.getCenter().lat();
-      controller.longitude = this.map.getCenter().lng();
+    controller.makeMap = function() {
+        controller.map = gMap(34.397, -118.644)
     }
-    controller.changedFeatured = function(tweet){
-      if (controller.featuredTweet != undefined){
-        controller.featuredTweet.closeBox();
-      }
-      controller.featuredTweet = tweet
-      tweet.flashBox();
-      $scope.data.style = {'background-image' : 'url('+ controller.featuredTweet.background_image +')'}
-    }
-    controller.flush = function(){
-      if (controller.featuredTweet != undefined){
-        controller.featuredTweet.closeBox();
-        controller.featuredTweet = null
-        $scope.data.style = {'background' : 'rgb(255, 120, 144)'}
-      }
-      for (var i = 0; i < controller.tweets.length; i++) {
-        controller.tweets[i].marker.setMap(null);
-      }
-      controller.tweets = [];
-    }
-    controller.search = { place: "Los Angeles", phrase: "anything" }
-    controller.codeAddress = function(){
-      new google.maps.Geocoder().geocode( { 'address': controller.search.place }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          controller.map.setCenter(results[0].geometry.location);
-        } else {
-          alert("Geocode was not successful for the following reason: " + status);
+    controller.changedFeatured = function(tweet) {
+        if (controller.featuredTweet != undefined) {
+            controller.featuredTweet.closeBox();
         }
-      });
+        controller.featuredTweet = tweet
+        tweet.flashBox();
+    }
+    controller.flush = function() {
+        if (controller.featuredTweet != undefined) {
+            controller.featuredTweet.closeBox();
+            controller.featuredTweet = null
+            $scope.data.style = {
+                'background': 'rgb(255, 120, 144)'
+            }
+        }
+        for (var i = 0; i < controller.tweets.length; i++) {
+            controller.tweets[i].marker.setMap(null);
+        }
+        controller.tweets = [];
+    }
+    controller.search = {
+        place: "Los Angeles",
+        phrase: "anything"
+    }
+    controller.codeAddress = function() {
+        new google.maps.Geocoder().geocode({
+            'address': controller.search.place
+        }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                controller.map.setCenter(results[0].geometry.location);
+            } else {
+                alert("Geocode was not successful for the following reason: " + status);
+            }
+        });
     }
     $scope.data = {}
-    $scope.loadData = function () {
-      for (var i = 0; i < controller.tweets.length; i++) {
-        controller.tweets[i].marker.setMap(null);
-      }
-      controller.tweets = [];
-      controller.flush();
-      fetchAll(controller.search.phrase, controller.map, controller, $scope)
+    $scope.loadData = function() {
+        for (var i = 0; i < controller.tweets.length; i++) {
+            controller.tweets[i].marker.setMap(null);
+        }
+        controller.tweets = [];
+        controller.flush();
+        fetchAll(controller.search.phrase, controller.map, controller, $scope)
     };
-    angular.element(document).ready(function () { 
-      controller.makeMap()
-      $scope.loadData()
+    angular.element(document).ready(function() {
+        controller.makeMap()
+        $scope.loadData()
     });
-  });
+});
--- public/js/spot.js
+++ public/js/spot.js
@@ -1,158 +1,156 @@
 function Spot(latitude, longitude, profile_image, text, retweet_count, user, background_image, map) {
-  this.latitude = latitude;
-  this.longitude = longitude;
-  this.profile_image = profile_image;
-  this.text = text;
-  this.retweet_count = retweet_count;
-  this.user = user;
-  this.background_image = background_image;
-  this.map = map
-  this.marker = this.makeMarker(latitude, longitude, profile_image, retweet_count);
-  this.infoBox = this.makeBox(text);
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.profile_image = profile_image;
+    this.text = text;
+    this.retweet_count = retweet_count;
+    this.user = user;
+    this.background_image = background_image;
+    this.map = map
+    this.marker = this.makeMarker(latitude, longitude, profile_image, retweet_count);
+    this.infoBox = this.makeBox(text);
 }
 
 Spot.prototype.openBox = function() {
-  this.infoBox.open(this.map, this.marker);
+    this.infoBox.open(this.map, this.marker);
 };
 
 Spot.prototype.flashBox = function() {
-  this.openBox()
-  this.popBox()
+    this.openBox()
+    this.popBox()
 };
 
 Spot.prototype.closeBox = function() {
-  this.infoBox.close()
+    this.infoBox.close()
 }
 
 Spot.prototype.displayProfile = function(i) {
-  this.flashBox()
+    this.flashBox()
 }
 
 Spot.prototype.popBox = function() {
-  var _this = this;
-  window.setTimeout(function(){
-    _this.closeBox()
-  }, 10000);
+    var _this = this;
+    window.setTimeout(function() {
+        _this.closeBox()
+    }, 10000);
 }
 
-Spot.prototype.makeMarker = function(){
-  return new google.maps.Marker({
-      map: this.map,
-      title: 'Map Icons',
-      zIndex: 9,
-      icon: this.isHot(this.profile_image, this.retweet_count),
-      position: new google.maps.LatLng(this.latitude, this.longitude),
-      visible: true
-  });
+Spot.prototype.makeMarker = function() {
+    return new google.maps.Marker({
+        map: this.map,
+        title: 'Map Icons',
+        zIndex: 9,
+        icon: this.isHot(this.profile_image, this.retweet_count),
+        position: new google.maps.LatLng(this.latitude, this.longitude),
+        visible: true
+    });
 }
 
-Spot.prototype.hot = function(){
-  return "/images/1420351727_132117.ico"
+Spot.prototype.hot = function() {
+    return "/images/1420351727_132117.ico"
 }
 
-Spot.prototype.isHot = function(profile_image, retweetCount){
-  if(retweetCount > 2){
-    return this.hot()
-  }else{
-    return profile_image;
-  }
+Spot.prototype.isHot = function(profile_image, retweetCount) {
+    if (retweetCount > 2) {
+        return this.hot()
+    } else {
+        return profile_image;
+    }
 }
 
-Spot.build = function (data, map) {
-  return new Spot(
-    data.latitude, 
-    data.longitude,
-    data.profile_image, 
-    data.text, 
-    data.retweet_count, 
-    data.user, 
-    data.background_image, 
-    map
-  );
+Spot.build = function(data, map) {
+    return new Spot(
+        data.latitude,
+        data.longitude,
+        data.profile_image,
+        data.text,
+        data.retweet_count,
+        data.user,
+        data.background_image,
+        map
+    );
 };
 
-Spot.prototype.makeBox = function(text){
-  return new InfoBox({
-    content: '<div style="background-repeat:no-repeat;background:white; padding: 10px 30px; color:blue; border-radius:150px; ">'+text+'</div>'
-    ,disableAutoPan: false
-    ,maxWidth: 0
-    ,pixelOffset: new google.maps.Size(-140, 0)
-    ,zIndex: null
-    ,boxStyle: {
-     }
-    ,closeBoxMargin: "10px 2px 2px 2px"
-    ,closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif"
-    ,infoBoxClearance: new google.maps.Size(1, 1)
-    ,isHidden: false
-    ,pane: "floatPane"
-    ,enableEventPropagation: false
-  });
+Spot.prototype.makeBox = function(text) {
+    return new InfoBox({
+        content: '<div style="background-repeat:no-repeat;background:white; padding: 10px 30px; color:blue; border-radius:150px; ">' + text + '</div>',
+        disableAutoPan: false,
+        maxWidth: 0,
+        pixelOffset: new google.maps.Size(-140, 0),
+        zIndex: null,
+        boxStyle: {},
+        closeBoxMargin: "10px 2px 2px 2px",
+        closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
+        infoBoxClearance: new google.maps.Size(1, 1),
+        isHidden: false,
+        pane: "floatPane",
+        enableEventPropagation: false
+    });
 }
 
- app.factory('mapi', function () {
-   return function(lat, lng){
-    var mapOptions = {
-      zoom: 8,
-      center: new google.maps.LatLng(lat, lng),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById("map-canvas"), this.mapOptions);
-    return map
-   }
- }); 
- 
- app.factory('spotFactory', function () {
-   return function(data, map){
-     return new Spot.build(data, map)
-   }
- });
+app.factory('gMap', function() {
+    return function(latitude, longitude) {
+        var mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(latitude, longitude),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        return new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    }
+});
 
- app.provider('Twitter', function(){
-   this.$get = ['$resource', function($resource){
-     var Tweets = $resource('/twitter/:lat/:lng/:phrase', {});
-     return Tweets
-   }];
- });
+app.factory('spotFactory', function() {
+    return function(data, map) {
+        return new Spot.build(data, map)
+    }
+});
 
- app.factory('momo', ['$resource', 'spotFactory', 'Twitter', 'mapi', '$q',function ($resource, spotFactory, Twitter, mapi, $q) {
-   var momo = {
-     async: function(lat, lng, phrased){
-       plomize = $q.defer();
-       var t = new Twitter.query({ lat: lat, lng: lng, phrase: phrased }, function(things){
-         plomize.resolve(things)
-       })
-       return plomize.promise
-     }
-  }
-  return momo
- }]);
- 
-app.factory('fetchAll', ['$resource', 'spotFactory', 'Twitter', 'mapi', 'momo', 'setupSpots', function ($resource, spotFactory, Twitter, mapi, momo, setupSpots) {
-  return function(phrased, map, list, $scope){
-     momo.async(map.getCenter().lng(), map.getCenter().lat(), phrased).then(function(resul){
-     var shreak = setupSpots(resul, map, list, $scope)
-      list.featuredTweet = list.tweets[0];
-      $scope.data.style = {'background-image' : 'url('+ controller.featuredTweet.background_image +')'}      
-      return shreak
-    })
-  }
-}]);
+app.provider('twitterSinatraApi', function() {
+    this.$get = ['$resource', function($resource) {
+        var Tweets = $resource('/twitter/:lat/:lng/:phrase', {});
+        return Tweets
+    }];
+});
 
-app.factory('setupSpots', ['$resource', 'spotFactory', function ($resource, spotFactory) {
-  return function(data, map, controller, $scope){
-    return data.map(function(x){
-      var spiz = spotFactory(x, map)
-      controller.tweets.push(spiz)
-      google.maps.event.addListener(spiz.marker,'click', function(e){
-        if (controller.featuredTweet != undefined){
-          controller.featuredTweet.closeBox()
+app.factory('Query', ['$resource', 'spotFactory', 'twitterSinatraApi', '$q', function($resource, spotFactory, twitterSinatraApi, $q) {
+    var Query = {
+        fetch: function(lat, lng, phrased) {
+            query = $q.defer();
+            t = new twitterSinatraApi.query({
+                lat: lat,
+                lng: lng,
+                phrase: phrased
+            }, function(tweets) {
+                query.resolve(tweets)
+            })
+            return query.promise
         }
-       spiz.flashBox();
-       controller.featuredTweet = spiz;
-       $scope.data.style = {'background-image' : 'url('+ controller.featuredTweet.background_image +')'}
-       $scope.$apply()
-      });
-    })
-  }
+    }
+    return Query
 }]);
- 
+
+app.factory('fetchAll', ['$resource', 'spotFactory', 'Query', 'setupSpots', function($resource, spotFactory, Query, setupSpots) {
+    return function(keywords, map, list, $scope) {
+        Query.fetch(map.getCenter().lng(), map.getCenter().lat(), keywords).then(function(result) {
+            setupSpots(result, map, list, $scope)
+            list.featuredTweet = list.tweets[0];
+        })
+    }
+}]);
+
+app.factory('setupSpots', ['$resource', 'spotFactory', function($resource, spotFactory) {
+    return function(data, map, controller, $scope) {
+        return data.map(function(tweetData) {
+            var spot = spotFactory(tweetData, map)
+            controller.tweets.push(spot)
+            google.maps.event.addListener(spot.marker, 'click', function(e) {
+                if (controller.featuredTweet != undefined) {
+                    controller.featuredTweet.closeBox()
+                }
+                spot.flashBox();
+                controller.featuredTweet = spot;
+                $scope.$apply()
+            });
+        })
+    }
+}]);
